33b61a4966855d9b01e4f480fdf36b1482a8b79b
Baxtiyor <Baxtiyor@yandex.ru>
2023-04-26 11:48:02 +0300

    a

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:music_instruments/src/pages/main_page.dart';
+import 'package:screensize_utils/screensize_util.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-double h = 0, w = 0, o = 0;
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,16 +14,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MainPage(),
-      builder: (BuildContext context, Widget? child) {
-        return MediaQuery(
-          data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
-          child: child!,
+    return ScreenSizeUtilInit(
+      designSize: const Size(320, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, state) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const MainPage(),
+          builder: (BuildContext context, Widget? child) {
+            return MediaQuery(
+              data: MediaQuery.of(context).copyWith(textScaleFactor: 1),
+              child: child!,
+            );
+          },
         );
       },
     );
--- lib/src/pages/catalog.dart
+++ lib/src/pages/catalog.dart
@@ -2,8 +2,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:music_instruments/src/helpers/apptheme.dart';
-
-import '../../main.dart';
+import 'package:screensize_utils/screensize_util.dart';
 
 class CatalogPage extends StatefulWidget {
   const CatalogPage({super.key});
@@ -15,9 +14,6 @@ class CatalogPage extends StatefulWidget {
 class _CatalogPageState extends State<CatalogPage> {
   @override
   Widget build(BuildContext context) {
-    h = MediaQuery.of(context).size.height / 400;
-    w = MediaQuery.of(context).size.width / 700;
-    o = (h + w) / 2;
     return Container(
       decoration: const BoxDecoration(
           image: DecorationImage(
@@ -30,50 +26,52 @@ class _CatalogPageState extends State<CatalogPage> {
           body: ListView(
             children: [
               // Container(
-              //             width: 215 * w,
-              //             height: 146 * h,
+              //             width: 215 . w,
+              //             height: 146 . h,
               //             decoration: BoxDecoration(
-              //                 borderRadius: BorderRadius.circular(12 * o),
+              //                 borderRadius: BorderRadius.circular(12 . o),
               //                 color: AppTheme.border),
               //           ),
               Container(
                 alignment: Alignment.center,
-                // margin: EdgeInsets.symmetric(horizontal: 30 * w),
-                height: 150 * h,
+                // margin: EdgeInsets.symmetric(horizontal: 30 . w),
+                height: 150.h,
                 child: ListView.separated(
-                    separatorBuilder: (_, index) => SizedBox(width: 20 * w),
+                    separatorBuilder: (_, index) => SizedBox(width: 20.w),
                     scrollDirection: Axis.horizontal,
                     shrinkWrap: true,
                     itemCount: 3,
                     itemBuilder: (_, index) => Container(
-                          width: 210 * w,
-                          height: 146 * h,
+                          width: 210.w,
+                          height: 146.h,
                           decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12 * o),
+                              borderRadius: BorderRadius.circular(12.r),
                               color: AppTheme.border),
                         )),
               ),
-              SizedBox(height: 55 * h),
+              SizedBox(height: 55.h),
               Container(
                 alignment: Alignment.center,
-                // margin: EdgeInsets.symmetric(horizontal: 30 * w),
-                height: 130 * h,
+                // margin: EdgeInsets.symmetric(horizontal: 30 . w),
+                height: 130.h,
                 child: ListView.separated(
-                    separatorBuilder: (_, index) => SizedBox(width: 20 * w),
+                    separatorBuilder: (_, index) => SizedBox(width: 20.w),
                     scrollDirection: Axis.horizontal,
                     shrinkWrap: true,
                     itemCount: 10,
                     itemBuilder: (_, index) => Column(
                           children: [
                             Container(
-                              width: 100 * w,
-                              height: 100 * h,
+                              width: 100.w,
+                              height: 100.h,
                               decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(12 * o),
+                                  borderRadius: BorderRadius.circular(12.r),
                                   color: AppTheme.border),
                             ),
-                            SizedBox(height: 6 * h),
-                            const Text('O‘zbek cholg‘ulari tarixi',)
+                            SizedBox(height: 6.h),
+                            const Text(
+                              'O‘zbek cholg‘ulari tarixi',
+                            )
                           ],
                         )),
               ),
